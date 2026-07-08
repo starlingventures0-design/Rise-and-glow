bacab132ceb3d84989c55da916c7286a71f0925a
starlingventures0-design <starlingventures0@gmail.com>
2026-07-08 21:05:42 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2138,7 +2138,7 @@ export default function App() {
                       })}
                     </div>
                   );
-                })()}
+                })} 
               </div>
             )}
           </div>

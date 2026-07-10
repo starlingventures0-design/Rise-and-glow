a79ab8a7eb498fdc6bc54d628c0f7c241c6df0da
starlingventures0-design <starlingventures0@gmail.com>
2026-07-10 02:27:57 +0100

    Remove unnecessary closing div tag

--- src/App.tsx
+++ src/App.tsx
@@ -3292,7 +3292,6 @@ className="w-10 h-10 bg-pink-500 hover:bg-pink-600 text-white rounded-full flex 
                 🌸 {customToast}
               </div>
       )}
-    </div>
   );
 }
                                    
